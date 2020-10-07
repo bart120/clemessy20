6a8a5202a60b7cf45abdfd5184a93614f20cd3b5
Vincent <vleclerc@inow.fr>
2020-10-07 16:29:40 +0200

    behaviorsubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserModel } from './models/user.model';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EIFFAGE ENERGIE SYSTEMES';
+  user: UserModel;
+
+  constructor(private servAuth: AuthService) { }
+
+  ngOnInit(): void {
+
+    console.log('subscribe');
+    this.servAuth.user$.subscribe(user => {
+      console.log('next app', user);
+      this.user = user;
+    });
+
+
+  }
+
+  onLogout(): void {
+    this.servAuth.logout();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { UserModel } from '../models/user.model';
 import { LoginService } from './login.service';
 
@@ -7,19 +9,34 @@ export class AuthService {
 
     private _user: UserModel = null;
 
+    private sujet: BehaviorSubject<UserModel> = new BehaviorSubject<UserModel>(null);
+
     get user(): UserModel {
         return this._user;
     }
 
-    constructor(private servLog: LoginService) { }
+    get user$(): Observable<UserModel> {
+        return this.sujet.asObservable();
+    }
+
+    constructor(private servLog: LoginService, private router: Router) {
+        this._user = JSON.parse(sessionStorage.getItem('USER')) as UserModel;
+        this.sujet.next(this._user);
+        console.log('next constructeur');
+    }
 
     login(login: string, password: string): void {
         this.servLog.login(login, password).subscribe(data => {
-            console.log(data);
-        })
+            this._user = { email: login, token: data.access_token };
+            sessionStorage.setItem('USER', JSON.stringify(this._user));
+            this.sujet.next(this._user);
+        });
     }
 
     logout(): void {
-
+        this._user = null;
+        sessionStorage.removeItem('USER');
+        this.sujet.next(null);
+        this.router.navigate(['/']);
     }
 }
